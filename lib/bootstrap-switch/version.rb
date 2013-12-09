772eb4ef4f1b046d3515705be179671455dd582d
Michael Hellein <michael.hellein@dealer.com>
2013-12-09 11:39:54 -0500

    Release v1.9.0

--- lib/bootstrap-switch/version.rb
+++ lib/bootstrap-switch/version.rb
@@ -2,7 +2,7 @@ module Bootstrap
   module Switch
     # This is updated via the `grunt bump` command, which has a pretty 
     # unflexible matching syntax.
-    VERSION_STRING = "'version': '1.8.0'"
+    VERSION_STRING = "'version': '1.9.0'"
     # Then, just the version.
     VERSION = VERSION_STRING.match(/\d+\.\d+\.\d+/)[0]
   end
